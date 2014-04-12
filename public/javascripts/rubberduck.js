a51694335875a6be341b3a6842f06ba3e7683684
Modsky1 <maude.jake@gmail.com>
2014-04-12 14:22:08 -0400

    return of text functioning

--- public/javascripts/rubberduck.js
+++ public/javascripts/rubberduck.js
@@ -11,3 +11,24 @@ $(document).ready(function() {
   $("#command_line").fadeIn(3000);
   $("form").submit(function() {
     var input = $("#command_line").val();
+
+
+    if (input.indexOf("regex") > -1) {
+      $("#question1").clone().insertBefore("#placeholder").fadeIn(1000);
+    }
+
+    if (input.indexOf("help") > -1) {
+      $("#hi").clone().insertBefore("#placeholder").fadeIn(1000);
+    }
+    if (input.indexOf("help") > -1) {
+      $("#hi").clone().toLowerCase().insertBefore("#placeholder").fadeIn(1000);
+    }
+
+
+
+    $("#console").scrollTop($("#console")[0].scrollHeight);
+    $("#command_line").val("");
+    });
+
+});
+
