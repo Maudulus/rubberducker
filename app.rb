275680c4b9054b5632d0724917a9fb36ab87c649
Modsky1 <maude.jake@gmail.com>
2014-04-12 11:24:21 -0400

    added postgres and questions table

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './environments.rb'
 class RubberDucker < ActiveRecord::Base
+
 end
--- environments.rb
+++ environments.rb
@@ -1,10 +1,12 @@
-db = URI.parse('postgres://user:pass@localhost/rubberducker')
+configure :production, :development, :test do
+  db = URI.parse('postgres://localhost/rubberducker')
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
